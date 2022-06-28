6cb0efc8adfe6dda7211bc6d4412cf0697317a2b
Gabe Harris <gabeharris@gmail.com>
2022-06-28 05:30:47 -0400

    course lesson 88

---
+++ src/routes/navigation-bar/NavigationBar.js
@@ -0,0 +1,31 @@
+import { Outlet, Link } from "react-router-dom";
+import { Fragment } from "react";
+import "./navigation.scss";
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+
+const NavigationBar = () => {
+  return (
+    <Fragment>
+      <header>
+        <div className="navigation">
+          <Link className="logo-container" to="/">
+            <CrwnLogo className="logo" />
+          </Link>
+          <div className="nav-links-container">
+            <Link className="nav-link" to="/shop">
+              Shop
+            </Link>
+            <Link className="nav-link" to="/sign-in">
+              Sign In
+            </Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </Fragment>
+  );
+};
+
+export default NavigationBar;
